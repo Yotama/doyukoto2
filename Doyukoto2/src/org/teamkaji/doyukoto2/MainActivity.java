2a6050e9c14ff42867d419dc1bd2b5e136ebb202
Yotama <tapo.tam@gmail.com>
2013-02-17 18:02:36 +0900

    認識エラー対応

--- Doyukoto2/src/org/teamkaji/doyukoto2/MainActivity.java
+++ Doyukoto2/src/org/teamkaji/doyukoto2/MainActivity.java
@@ -28,8 +28,14 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * メインアクティビティ
+ * @author yotama
+ *
+ */
 public class MainActivity extends Activity implements RecognitionListener {
     private static final int VOICE_REQUEST_CODE = 123;
     
@@ -51,8 +57,8 @@ public class MainActivity extends Activity implements RecognitionListener {
         
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
 
-//        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-//        speechRecognizer.setRecognitionListener(this);
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(this);
 
     }
 
@@ -63,22 +69,31 @@ public class MainActivity extends Activity implements RecognitionListener {
         return true;
     }
 
+    /**
+     * 音声認識インテント発行
+     */
     private void voiceSearch() {
         try {
             // インテント作成
             Intent intent = new Intent(
                     RecognizerIntent.ACTION_RECOGNIZE_SPEECH); // ACTION_WEB_SEARCH
-            intent.putExtra(
-                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(
-                    RecognizerIntent.EXTRA_PROMPT,
-                    "VoiceRecognitionTest");
+//            intent.putExtra(
+//                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//            intent.putExtra(
+//                    RecognizerIntent.EXTRA_PROMPT,
+//                    "VoiceRecognitionTest");
+            
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                            getPackageName());
             
             // インテント発行
-            startActivityForResult(intent, VOICE_REQUEST_CODE);
-//            speechRecognizer.startListening(RecognizerIntent.getVoiceDetailsIntent(getApplicationContext()));
-            
+//            startActivityForResult(intent, VOICE_REQUEST_CODE);
+            speechRecognizer.startListening(intent);
+            Log.d("voice", "認識中");
+            setStatusText("認識中");
         } catch (ActivityNotFoundException e) {
             // このインテントに応答できるアクティビティがインストールされていない場合
             Toast.makeText(this,
@@ -86,29 +101,37 @@ public class MainActivity extends Activity implements RecognitionListener {
         }
     }
     
+    /**
+     * アクティビティ結果処理
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // 自分が投げたインテントであれば応答する
         if (requestCode == VOICE_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (resultCode == RESULT_OK) {
+                // 結果文字列リスト
+                ArrayList<String> results = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                
+                // トーストを使って結果を表示
+                Toast.makeText(this, results.get(0), Toast.LENGTH_LONG).show();
+                EditText accountText = (EditText) this.findViewById(R.id.editText1);
+                
+                postData(accountText.getText().toString(), results.get(0));
+            }
             
-            // 結果文字列リスト
-            ArrayList<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            
-            // トーストを使って結果を表示
-            Toast.makeText(this, results.get(0), Toast.LENGTH_LONG).show();
-            EditText accountText = (EditText) this.findViewById(R.id.editText1);
-            accountText.getText();
-            
-            postData(accountText.getText().toString(), results.get(0));
             voiceSearch();
-        } else if (requestCode == VOICE_REQUEST_CODE && resultCode == RecognizerIntent.RESULT_NO_MATCH) {
-            Toast.makeText(this, resultCode, Toast.LENGTH_LONG).show();
         }
         
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    /**
+     * テキストをサーバにポストする
+     * 
+     * @param account
+     * @param text
+     */
     private void postData(String account, String text) {
 //        HttpPost request = new HttpPost("http://cd4tdik-aj2-app000.c4sa.net/register.php");
         HttpPost request = new HttpPost("http://210.129.194.74/pushTalk.php");
@@ -125,7 +148,7 @@ public class MainActivity extends Activity implements RecognitionListener {
         new HttpPostTask().execute(request);
         
     }
-    
+
     class HttpPostTask extends AsyncTask<HttpUriRequest, Void, HttpResponse> {
         // doInBackground() に、バックグラウンド処理の内容を記述する。
         // ここではAndroidHttpClientによるHTTP GET実行
@@ -140,8 +163,6 @@ public class MainActivity extends Activity implements RecognitionListener {
             return response;
         }
         
-        // onPostExecute() に、バックグラウンド処理完了時の処理を記述する。
-        // ここでは、HTTPレスポンスボディとして取得した文字列のTextViewへの貼り付け
         protected void onPostExecute(HttpResponse response) {
             try {
                 Log.d("================", EntityUtils.toString(response.getEntity()));
@@ -155,42 +176,57 @@ public class MainActivity extends Activity implements RecognitionListener {
         }
     }
     
+    /**
+     * エラーコールバック
+     */
     @Override
     public void onError(int error) {
+        String errorMsg = null;
         switch (error) {
         case SpeechRecognizer.ERROR_AUDIO:
             // 音声データ保存失敗
+            errorMsg = "error audio";
             break;
         case SpeechRecognizer.ERROR_CLIENT:
             // Android端末内のエラー(その他)
+            errorMsg = "error client";
             break;
         case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
             // 権限無し
+            errorMsg = "no permissions";
             break;
         case SpeechRecognizer.ERROR_NETWORK:
             // ネットワークエラー(その他)
-            Log.e("voice", "network error");
+            errorMsg = "network error";
             break;
         case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
             // ネットワークタイムアウトエラー
             Log.e("voice", "network timeout");
+            errorMsg = "no permissions";
             break;
         case SpeechRecognizer.ERROR_NO_MATCH:
             // 音声認識結果無し
-            Toast.makeText(this, "no match Text data", Toast.LENGTH_LONG).show();
+            errorMsg = "no match";
             break;
         case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
             // RecognitionServiceへ要求出せず
+            errorMsg = "recognizer busy";
             break;
         case SpeechRecognizer.ERROR_SERVER:
             // Server側からエラー通知
+            errorMsg = "server error";
             break;
         case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
             // 音声入力無し
-            Toast.makeText(this, "no input?", Toast.LENGTH_LONG).show();
+            errorMsg = "speech timeout";
             break;
         default:
         }
+        
+        Log.e("voice", errorMsg);
+        Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
+        setStatusText(errorMsg);
+        voiceSearch();
     }
 
     @Override
@@ -224,20 +260,47 @@ public class MainActivity extends Activity implements RecognitionListener {
 
     @Override
     public void onReadyForSpeech(Bundle arg0) {
-        Toast.makeText(this, "onReadyForSpeech", Toast.LENGTH_LONG).show();
-        
+        Log.d("voice", "準備できたよん");
     }
 
+    /**
+     * 認識後処理
+     */
     @Override
     public void onResults(Bundle results) {
         ArrayList<String> recData = results
                 .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        Toast.makeText(this, "aaaaaaaaaaaaa" + recData.get(0), Toast.LENGTH_LONG).show();
+        Log.d("voice", recData.get(0));
+        Toast.makeText(this, recData.get(0), Toast.LENGTH_LONG).show();
+
+        setTermText(recData.get(0));
+
+        EditText accountText = (EditText) this.findViewById(R.id.editText1);
+        postData(accountText.getText().toString(), recData.get(0));
+        voiceSearch();
     }
 
     @Override
     public void onRmsChanged(float arg0) {
         // TODO Auto-generated method stub
         
+    }
+    
+    /**
+     * ステータス表示変更
+     * @param status
+     */
+    private void setStatusText(String status) {
+        TextView statusText = (TextView) findViewById(R.id.textView_status);
+        statusText.setText("ステータス：" + status);
+    }
+    
+    /**
+     * 認識文を表示する
+     * @param text
+     */
+    private void setTermText(String text) {
+        TextView termText = (TextView) this.findViewById(R.id.textView_term);
+        termText.setText(text);
     }
 }
